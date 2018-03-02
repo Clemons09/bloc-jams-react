781cc157edaaee53a1568042d118522c5fb49456
Clemons09 <clemons9988@gmail.com>
2018-03-02 16:08:35 -0700

    audio playback cp complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -11,9 +11,38 @@ class Album extends Component {
       return album.slug === this.props.match.params.slug
     });
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
 
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
+  }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); }
+      this.play();
+    }
   }
 
   render() {
@@ -37,11 +66,11 @@ class Album extends Component {
 
         <tbody>
           {this.state.album.songs.map( (song, index) =>
-            <tr className="song" key={index}>
+            <tr className="song" key={index} onClick={() => this.handleSongClick(song)}>
               <td className="song-icons">
-              <Ionicon icon="ios-play"/>
-              <Ionicon icon="ios-pause"/>
-                  {index + 1}
+              <Ionicon icon={this.state.isPlaying ? 'ios-pause' : 'ios-play'}>
+                {index + 1}
+              </Ionicon>
               </td>
               <td className="song-title">{song.title}</td>
               <td className="song-duration">{song.duration}</td>
